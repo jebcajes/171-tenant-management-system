d88ee26bcc4307b017e9c4e901e836bdfc092e77
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-01-20 13:20:20 +0800

    price history added

--- admin-view-price-history.php
+++ admin-view-price-history.php
@@ -47,13 +47,83 @@
 <body>
     <div class="container">
         <br />
-        
+
         <div class="row">
             <div class="col">
-                sex
+                <h3 class="float-left">Previous</h3>
+                <table class="table table-sm table-bordered table-striped">
+                    <tr align="center">
+                        <th>Old Price</th>
+                        <th>Date End</th>
+                        <th>Date Effectivity</th>
+                    </tr>
+                    <?php 
+                        require_once "api/config.php";
+                        $stall_id = $_GET['stall_id'];
+
+                        $sql_history = "SELECT * FROM stall_pricehistory WHERE stall_id = $stall_id";
+                        $result_history = mysqli_query($link, $sql_history);
+                        if(mysqli_num_rows($result_history) > 0){
+                            while($row_history = mysqli_fetch_assoc($result_history)){
+                                echo "<tr align='center'>";
+                                    echo "<td>Php " . number_format($row_history['stall_price'],2) . "</td>";
+                                    $old_date_end = strtotime($row_history['date_end']);
+                                    $new_date_end = date('Y-m-d', $old_date_end);
+                                    echo "<td>" . $new_date_end . "</td>";
+                                    $old_date_effectivty = strtotime($row_history['date_effectivity']);
+                                    $new_date_effectivity = date('Y-m-d', $old_date_effectivty);
+                                    echo "<td>" . $new_date_effectivity . "</td>";
+                                echo "</tr>";
+                            }
+                        }else{
+                            echo "<tr align='center'>";
+                                echo "<td colspan='4' style='font-style: italic;'>No records found.</td>";
+                            echo "</tr>";
+                        }
+                    ?>
+                </table>
             </div>
             <div class="col">
-                yea
+                <h3 class="float-left">Current</h3>
+                <a href="admin-stalls.php" class="btn btn-danger btn-sm float-right">Back</a>
+                
+                <table class="table table-sm table-bordered table-striped">
+                    <tr>
+                        <th>#</th>
+                        <th>Floor</th>
+                        <th>Block</th>
+                        <th>Block Dimension</th>
+                        <th>Current Price</th>
+                        <th>Date Effectivity</th>
+                        <?php
+
+                        $sql_stalls = "SELECT * FROM stalls WHERE stall_id = $stall_id";
+
+                        $result_stalls = mysqli_query($link, $sql_stalls);
+                        if(mysqli_num_rows($result_stalls) > 0 ){
+                            while($row_stalls = mysqli_fetch_assoc($result_stalls)){
+                                $floor_no = $row_stalls['floor_no'];
+                                $block_no = $row_stalls['block_no'];
+                                $block_dimension = $row_stalls['block_dimension'];
+                                $stall_price = $row_stalls['stall_price'];
+                                $price_date_effectivity = $row_stalls['price_date_effectivity'];
+                                $stall_id = $row_stalls['stall_id'];
+
+                                echo "<tr align='center' style='font-size: 15px;'>";
+                                    echo "<td>" . $stall_id . "</td>";
+                                    echo "<td>" . $floor_no . "</td>";
+                                    echo "<td>" . $block_no . "</td>";
+                                    echo "<td>" . $block_dimension . "</td>";
+                                    echo "<td>Php " . number_format($stall_price,2) . "</td>";
+                                    $old_price_date = strtotime($price_date_effectivity);
+                                    $new_price_date = date('Y-m-d', $old_price_date);
+                                    echo "<td>" . $new_price_date . "</td>";
+                                echo "</tr>";
+                            }
+                        }
+                        ?>
+                    </tr>
+                </table>
             </div>
         </div>
     </div>
--- api/admin-update-stall.php
+++ api/admin-update-stall.php
@@ -8,9 +8,9 @@
     $old_price_date = strtotime($price_date_effectivity);
     $new_price_date = date('Y-m-d H:i:s', $old_price_date);
     
-    if(!empty($stall_price)){
+    if(!empty($stall_price) && !empty($price_date_effectivity)){
         $sql_update_price = "UPDATE stalls
-        SET stall_price = $stall_price
+        SET stall_price = $stall_price, price_date_effectivity = '$price_date_effectivity'
         WHERE stall_id = $stall_id";
         if(mysqli_query($link, $sql_update_price)){
             echo "Price updated successfully! <br />";
@@ -18,17 +18,7 @@
         }
     }
 
-    if(!empty($price_date_effectivity)){
-        $sql_update_date = "UPDATE stalls
-        SET price_date_effectivity = '$price_date_effectivity'
-        WHERE stall_id = $stall_id";
-        if(mysqli_query($link, $sql_update_date)){
-            echo "Date updated successfully! <br />";
-            $b++;
-        }
-    }
-
-    if($a || $b == 1){
+    if($a == 1){
         // Redirect back to the page
         header("Refresh: 2; url= ../admin-stalls.php");
     }
